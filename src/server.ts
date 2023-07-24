2d82cb2d745bb7946fa126af4ae122d3c5b33cdb
Leandro Grande <leandrowg@gmail.com>
2023-07-24 06:17:54 -0300

    feat: add error handler

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,20 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import { env } from './env';
+import { ZodError } from 'zod';
+import { AppError } from './util/AppError';
 
 const app = express();
+
+app.use(express.json());
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+	if (err instanceof AppError) {
+		return response.status(err.statusCode).json({error: err.message});
+	} else if (err instanceof ZodError)  {
+		return response.json({ error: err.format() });
+	}
+
+	return response.status(500).json({status: 'error', message: 'Internal server error'});
+});
 
 app.listen(env.PORT, () => console.log(`Server is running! on port ${env.PORT} ✅`));
