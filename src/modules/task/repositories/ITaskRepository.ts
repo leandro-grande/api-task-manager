2c01db5ba92a42da332adfcef110a7c5cc02082f
Leandro Grande <leandrowg@gmail.com>
2023-07-28 14:00:58 -0300

    feat: add update on TaskRepository

--- src/modules/task/repositories/ITaskRepository.ts
+++ src/modules/task/repositories/ITaskRepository.ts
@@ -4,4 +4,5 @@ import { Prisma, Task } from '@prisma/client';
 export interface ITaskRepository {
 	create(data: Prisma.TaskUncheckedCreateInput): Promise<Task>;
 	findById(taskId: string): Promise<Task>;
+	update(taskId: string, completed: boolean): Promise<Task>;
 }
--- src/modules/task/repositories/taskRepository.ts
+++ src/modules/task/repositories/taskRepository.ts
@@ -2,8 +2,8 @@ import { Prisma } from '@prisma/client';
 import { ITaskRepository } from './ITaskRepository';
 import { prisma } from '../../../database/prisma';
 
-
 export class TaskRepository implements ITaskRepository {
+
 	async create(data: Prisma.TaskUncheckedCreateInput){
 		const task = await prisma.task.create({
 			data: {
@@ -23,6 +23,19 @@ export class TaskRepository implements ITaskRepository {
 		const task = await prisma.task.findFirst({
 			where: {
 				id: taskId
+			}
+		});
+
+		return task;
+	}
+
+	async update(taskId: string, completed: boolean) {
+		const task = await prisma.task.update({
+			where: {
+				id: taskId
+			},
+			data: {
+				completed
 			}
 		});
 
